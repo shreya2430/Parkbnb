4c0895c706c103f9bca43cb42fa703d922bb7992
dharanakneu <kashyap.dh@northeastern.edu>
2024-11-07 20:21:40 -0500

    Add onCreate and onUpdate triggers

--- src/main/java/edu/neu/csye6200/parkingapp/model/BaseEntity.java
+++ src/main/java/edu/neu/csye6200/parkingapp/model/BaseEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import jakarta.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -14,11 +15,22 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "created_at", columnDefinition = "TIMESTAMPTZ DEFAULT NOW()")
+    @Column(name = "created_at", columnDefinition = "TIMESTAMPTZ DEFAULT NOW()", updatable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at", columnDefinition = "TIMESTAMPTZ DEFAULT NOW()")
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 
     public Long getId() {
         return id;
