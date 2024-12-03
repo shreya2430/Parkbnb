f98ecca48b348517cb4231d959326960d4fddde8
dharanakneu <kashyap.dh@northeastern.edu>
2024-12-02 21:59:43 -0500

    Get parking location and spot data with the reservations

--- src/main/java/edu/neu/csye6200/parkingapp/dto/ReservationDTO.java
+++ src/main/java/edu/neu/csye6200/parkingapp/dto/ReservationDTO.java
@@ -1,9 +1,7 @@
 package edu.neu.csye6200.parkingapp.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
 import java.sql.Timestamp;
-
 
 public class ReservationDTO {
     private Long id;
@@ -14,15 +12,9 @@ public class ReservationDTO {
     @NotBlank(message = "End Time is required")
     private Timestamp endTime;
 
-    @NotBlank(message = "Confirmation code is required")
-    @Size( max = 10, message = "Confirmation code  must be of 10 characters")
-    private String confirmationCode;
-
     @NotBlank(message = "Status is required")
     @Size( max = 15, message = "Status can have max 15 characters")
     private String status;
-
-    private Boolean isEmailSent;
 
     @NotBlank(message = "Rentee ID is required")
     private Long renteeId;
@@ -32,6 +24,8 @@ public class ReservationDTO {
 
     @NotBlank(message = "Payment ID is required")
     private Long paymentId;
+
+    private ParkingSpot parkingSpot;
 
     public Long getId() {
         return id;
@@ -57,28 +51,12 @@ public class ReservationDTO {
         this.endTime = endTime;
     }
 
-    public String getConfirmationCode() {
-        return confirmationCode;
-    }
-
-    public void setConfirmationCode(String confirmationCode) {
-        this.confirmationCode = confirmationCode;
-    }
-
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public Boolean getEmailSent() {
-        return isEmailSent;
-    }
-
-    public void setEmailSent(Boolean emailSent) {
-        isEmailSent = emailSent;
     }
 
     public Long getRenteeId() {
@@ -103,5 +81,110 @@ public class ReservationDTO {
 
     public void setPaymentId(Long paymentId) {
         this.paymentId = paymentId;
+    }
+
+    public ParkingSpot getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(ParkingSpot parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public static class ParkingSpot{
+        private String spotNumber;
+        ParkingLocation parkingLocation;
+
+        public String getSpotNumber() {
+            return spotNumber;
+        }
+
+        public void setSpotNumber(String spotNumber) {
+            this.spotNumber = spotNumber;
+        }
+
+        public ParkingLocation getParkingLocation() {
+            return parkingLocation;
+        }
+
+        public void setParkingLocation(ParkingLocation parkingLocation) {
+            this.parkingLocation = parkingLocation;
+        }
+    }
+
+
+    public static class ParkingLocation{
+        private String street;
+        private String city;
+        private String state;
+        private String postalcode;
+        private String country;
+        private String latitude;
+        private String longitude;
+        private String parkingLocationImage;
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public String getPostalcode() {
+            return postalcode;
+        }
+
+        public void setPostalcode(String postalcode) {
+            this.postalcode = postalcode;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(String latitude) {
+            this.latitude = latitude;
+        }
+
+        public String getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(String longitude) {
+            this.longitude = longitude;
+        }
+
+        public String getParkingLocationImage() {
+            return parkingLocationImage;
+        }
+
+        public void setParkingLocationImage(String parkingLocationImage) {
+            this.parkingLocationImage = parkingLocationImage;
+        }
     }
 }
