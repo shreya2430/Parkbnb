0aadac8364e5d33171ab594a8d2d41708f191fe9
dharanakneu <kashyap.dh@northeastern.edu>
2024-12-08 18:48:26 -0500

    Add logic for delete card

--- src/main/java/edu/neu/csye6200/parkingapp/controller/ParkingSpotController.java
+++ src/main/java/edu/neu/csye6200/parkingapp/controller/ParkingSpotController.java
@@ -57,5 +57,4 @@ public class ParkingSpotController {
         parkingSpotService.deleteParkingSpot(id);
     }
 
-
 }
--- src/main/java/edu/neu/csye6200/parkingapp/repository/PaymentRepository.java
+++ src/main/java/edu/neu/csye6200/parkingapp/repository/PaymentRepository.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Payment findByStripePaymentId(String stripePaymentId);
+
+    boolean existsByCardId(Long cardId);
 }
 
